f040ce0e72ed197c7613bcdabdf522d6a4f4c31e
ananthanil <ab.ananth97@gmail.com>
2022-10-20 00:02:04 +0530

    extension changed into html

---
+++ footer.php
@@ -0,0 +1,74 @@
+<htm lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" >
+    
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" />
+        <style>
+            .scrollbar::-webkit-scrollbar{
+                display: none;
+            }
+        </style>
+    </head>
+    <body>
+
+        <!-- FOOTER SECTION -->
+
+        <footer>
+            <div class="container-fluid  pb-2" style="background-color: black;">
+             
+                <div class="row text-center pt-5">
+                    <div class="col-12 col-md-4 mt-4  pt-4">
+                        <h5 class="text-info">More Links</h5>
+                        <a href="index.php" class="text-light d-block">Home</a>
+                        <a href="Yoga_index_page.php" class="text-light d-block">Yoga</a>
+                        <a href="#" class="text-light d-block">Blogs</a>
+                    </div>
+				
+                    <div class="col-12 col-md-4 text-light text-justify mt-3 pt-3 pb-1">
+                        <p>
+                            If it is for maintenance of good
+                            health to lead a happy and contented life practice
+                            of difficult yoga postures is not needed Warming
+                            up the body with some easy preliminary exercises
+                            a few postures of yoga by standing, sitting and
+                            lying and some eye exercises, breathing exercises
+                            (Pranayama) and abdomen exercises can be
+                            practiced by 20 or 25 minutes. This website is not
+                            designed as a guide for beginners. They must get
+                            the help of a master because the website is not
+                            capable of making any corrections when the
+                            beginner is going wrong. More over the
+                            practitioner will be always doubtful regarding the
+                            correctness of his postures. When a master gives
+                            confidence by commenting, the beginner will have
+                            a psychological effect. Unlike other methods, yoga
+                            is giving exercises for all parts of body. This needs
+                            no equipments. Therefor this can be practiced
+                            anywhere in the world if we get six feet space.
+                        </p>
+                    </div>
+				
+                    <div class="col-12 col-md-4 mt-4 pt-4 py-3">
+                        <h5 class="text-info pb-3">Social</h5>
+                        <a href="#"><i class="fab fa-facebook text-light h1 d-block"></i></a>
+                        <a href="https://www.youtube.com/channel/UCKsq6G6r_mUCMsDIPtdTDPw"><i class="fab fa-youtube text-light h1 d-block"></i></a>
+                        <a href="#"><i class="fab fa-instagram text-light h1 d-block"></i></a>
+                    </div>
+				</div>	
+            <div class="text-muted text-center bg-drak pt-2 pb-2">
+               <p>Copyright @ yogacharayan 2020 & Designed by Ananth Krishnan A</p>
+               
+             </div>
+            </div>
+        </footer>
+
+    </body>
+</html>
